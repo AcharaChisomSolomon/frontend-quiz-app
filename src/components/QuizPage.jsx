c4be6c1e2a4767e9b2588ca80188d209efce2ffe
Achara Chisom Solomon <ca0102c@gre.ac.uk>
2024-02-22 09:43:00 +0000

    Quizpage design implemented

--- src/components/QuizPage.jsx
+++ src/components/QuizPage.jsx
@@ -1,32 +1,58 @@
+import correctLogo from '../assets/images/icon-correct.svg';
+import wrongLogo from '../assets/images/icon-error.svg';
+
 const QuizPage = () => {
     return (
-        <div>
-            <p>Question 6 of 10</p>
-            <h3>
-                Which of these color contrast ratios defines the minimum 
-                WCAG 2.1 AA requirement for normal text?
-            </h3>
-            <div></div>
-            <div>
-                <button>
-                    <div>A</div>
-                    <div>4.5:1</div>
-                </button>
-                <button>
-                    <div>B</div>
-                    <div>3:1</div>
-                </button>
-                <button>
-                    <div>C</div>
-                    <div>2.5:1</div>
-                </button>
-                <button>
-                    <div>D</div>
-                    <div>5:1</div>
-                </button>
-            </div>
-            <button>Submit Answer</button>
+      <div>
+        <p className="quiz-text">Question 6 of 10</p>
+
+        <h3 className="quiz-question">
+          Which of these color contrast ratios defines the minimum WCAG 2.1 AA
+          requirement for normal text?
+        </h3>
+
+        <div className="quiz-visualNum__container">
+          <div className="quiz-visualNum"></div>
         </div>
+
+        <div className="quiz-options">
+          <button className="quiz-option selected">
+            <div className="quiz-option__tag">A</div>
+            <div className="quiz-option__text">4.5:1</div>
+          </button>
+          <button className="quiz-option correct">
+            <div className="quiz-option__tag">B</div>
+            <div className="quiz-option__text">3:1</div>
+            <div className="quiz-option__logo">
+              <img src={correctLogo} alt="Correct answer" />
+            </div>
+          </button>
+          <button className="quiz-option wrong">
+            <div className="quiz-option__tag">C</div>
+            <div className="quiz-option__text">2.5:1</div>
+            <div className="quiz-option__logo">
+              <img src={wrongLogo} alt="Wrong answer" />
+            </div>
+          </button>
+          <button className="quiz-option correct-not-selected">
+            <div className="quiz-option__tag">D</div>
+            <div className="quiz-option__text">5:1</div>
+            <div className="quiz-option__logo">
+              <img src={correctLogo} alt="Correct answer" />
+            </div>
+          </button>
+        </div>
+
+        <button className="quiz-submit">Submit Answer</button>
+        <div className='error'>
+            <div className='error-logo'>
+                <img src={wrongLogo} alt="An error messsage" />
+            </div>
+            <div className='error-text'>
+                <p>Please select an answer</p>
+            </div>
+        </div>
+      </div>
     );
  }
 

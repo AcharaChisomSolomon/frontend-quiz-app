a52d525c118077b74194553e2e020276e149eda2
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2024-05-12 15:17:55 +0100

    Refactor CSS for hover and active states, and import quiz data from JSON file

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+import data from './data.json' with { "type": "json" };
+const { quizzes } = data;
+
 //chosen
 //correct
 //incorrect
 //correct-but-not-chosen
+
 const themeSwitcher = document.getElementById('theme');
 const themeCircle = document.getElementById('circle');
 themeSwitcher.addEventListener('click', () => {
     document.body.classList.toggle('dark');
     themeCircle.style.marginLeft = themeCircle.style.marginLeft === '0px' ? 'auto' : '0px';
 });
+
+const quizContainer = document.getElementById('quiz-container');

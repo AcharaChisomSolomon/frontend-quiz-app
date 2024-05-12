51989fde49377b44d4ce87a55244fef26b83000c
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2024-05-12 15:08:42 +0100

    enable clicking between dark and light mode

--- index.js
+++ index.js
@@ -2,3 +2,9 @@
 //correct
 //incorrect
 //correct-but-not-chosen
+const themeSwitcher = document.getElementById('theme');
+const themeCircle = document.getElementById('circle');
+themeSwitcher.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    themeCircle.style.marginLeft = themeCircle.style.marginLeft === '0px' ? 'auto' : '0px';
+});
